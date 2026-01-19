ca21f6d187aefc919e42e198e0627fc9e6b0ed6c
Furqan Shaikh <furqanshaikh7864@gmail.com>
2026-01-19 17:28:54 +0530

    Add files via upload

---
+++ functions.java
@@ -0,0 +1,13 @@
+import arithmetic.arith;
+class functions
+{
+  public static void main(String arg[])
+  {
+   arith a=new arith();
+   a.add(10,20);
+   a.subtract(40,20);
+   a.multiply(4,8);
+   a.divide(4,20);
+   a.mod(4,30);
+  }
+}
